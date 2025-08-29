62d71001f417d1f42a5dfffa261b5e0118632ac8
Vishal Roy <vishalsyadav2005@gmail.com>
2025-08-29 12:10:41 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { assets } from "../assets/assets_frontend/assets";
+
+const Footer = () => {
+  return (
+    <div className="md:mx-10">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        {/* --------left section--------- */}
+        <div>
+          <img className="mb-5 w-40" src={assets.logo} alt="" />
+          <p className="w-full md:w-2/3 text-gray-600 leading-6">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum sunt
+            autem perferendis. Nam dignissimos totam ad? Possimus cupiditate a,
+            non eaque voluptatibus officiis sequi porro incidunt ratione, omnis
+            delectus? Quisquam!
+          </p>
+        </div>
+
+        {/* --------center section--------- */}
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Contact us</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        {/* --------right section--------- */}
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
+            <li>+11-221-456-7879</li>
+            <li>vishalsyadav2005@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+
+      {/* ----------copyright text------------ */}
+
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright 2024@ DocTech - All Right Reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
